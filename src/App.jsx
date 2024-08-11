9c03d547dc814e512474e847393e5768cf23fb1d
sarsur9 <rami.moon@outlook.com>
2024-08-11 23:17:30 +0300

    filter products shop

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
   Link,
 } from "react-router-dom";
 
- 
+
 
 const App = () => {
  const user = true;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,3 +1,6 @@
+import { SettingsRemoteOutlined } from "@material-ui/icons";
+import { useState } from "react";
+import { useLocation } from "react-router";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
@@ -21,14 +24,12 @@ const Filter = styled.div`
   margin: 20px;
   ${mobile({ margin: "0px 15px", display: "flex", flexDirection: "column" })}
 `;
-
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
   ${mobile({ marginRight: "0px" })}
 `;
-
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
@@ -37,18 +38,29 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+    const location = useLocation();
+    const cat = location.pathname.split("/")[2];
+    console.log(cat);
+    const [filters, setFilters] = useState({});
+    const [sort, setSort] = useState("newest");
+
+    const handleFilters = (e) => {
+      const value = e.target.value;
+      setFilters({
+        ...filters,
+        [e.target.name]: value,
+      });
+    };
   return (
     <Container>
-      <Announcement />
       <Navbar />
-      <Title>O.T.C</Title>
+      <Announcement />
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
-          <Select>
-            <Option disabled selected>
-              Pick a Category
-            </Option>
+          <Select name="category" onChange={handleFilters}>
+            <Option disabled>Pick a Category</Option>
             <Option>Cold and Flu</Option>
             <Option>Health</Option>
             <Option>Painkillers</Option>
@@ -57,17 +69,14 @@ const ProductList = () => {
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
-          <Select>
-            <Option disabled selected>
-              Sort By
-            </Option>
-            <Option>Most Recent</Option>
-            <Option>Price(from lowest)</Option>
-            <Option>Price(from highest)</Option>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products />
+      <Products cat={cat} filters={filters} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
